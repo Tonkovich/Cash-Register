95ec37f5b4036104b4ed92658a40e3da07c438eb
Tonk <a_tonkovich@yahoo.com>
2018-02-04 01:04:44 -0600

    Test cases now simulate a virtual user using scanner

--- src/net/tonkovich/tests/TestCases.java
+++ src/net/tonkovich/tests/TestCases.java
@@ -3,77 +3,99 @@ package net.tonkovich.tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.text.DecimalFormat;
-
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import net.tonkovich.java.Discount;
 import net.tonkovich.java.Discount10;
 import net.tonkovich.java.Discount20;
+import net.tonkovich.java.DiscountOptions;
+
+import org.junit.jupiter.api.Test;
 
 class TestCases {
-	
-	//TODO: Simulate virtual user
 
-	@Test
-	void discount10Test() {
-		Discount dis = new Discount();
-		dis.setDiscount(new Discount10()); // Assign option
-		DecimalFormat df = new DecimalFormat("#.##"); 
-		
-		double price = 2.52; // Assign test values
-		int quantity = 7;
-		
-		double value = dis.calculate(price, quantity);
-		
-		assertEquals("15.88", df.format(value), "Value should be 15.88");
-	}
-	
-	@Test
-	void discount20Test() {
-		Discount dis = new Discount();
-		dis.setDiscount(new Discount20()); // Assign option
-		DecimalFormat df = new DecimalFormat("#.##"); 
-		
-		double price = 2.52; // Assign test values
-		int quantity = 7;
-		
-		double value = dis.calculate(price, quantity);
-		
-		assertEquals("14.11", df.format(value), "Value should be 14.11");
-	}
-	
-	@Test
-	void totalTest() {
-		// 2.52/7/2/14.11
-		// 3/2/1/19.51
-		DecimalFormat df = new DecimalFormat("#.##");
-		Discount dis = new Discount();
-		dis.setDiscount(new Discount20());
-		
-		double price = 2.52;
-		int quantity = 7;
-		
-		double total = dis.calculate(price, quantity);
-		
-		dis.setDiscount(new Discount10());
-		
-		price = 3;
-		quantity = 2;
-		
-		total += dis.calculate(price, quantity);
-		
-		assertEquals("19.51", df.format(total), "Value should be 19.51");
-	}
-	
-	@Test
-	void userInputNullTest() {
-		
-	}
-	
-	@Test 
-	void userInputWrongTypeTest() {
-		
-	}
-	
-	
+  private List<DiscountOptions> dList = new ArrayList<DiscountOptions>() {
+    {
+      add(new Discount10());
+      add(new Discount20());
+    }
+  };
+
+  private Discount dis = Discount.getInstance();
+
+  @Test
+  void discount10Test() {
+    dis.setDiscount(new Scanner("1"), dList); // Assign option
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    dis.setUnitPrice(new Scanner("2.52"));
+    dis.setQuantity(new Scanner("7"));
+
+    double value = dis.calculate();
+
+    assertEquals("15.88", df.format(value), "Value should be 15.88");
+  }
+
+  @Test
+  void discount20Test() {
+    dis.setDiscount(new Scanner("2"), dList); // Assign option
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    dis.setUnitPrice(new Scanner("2.52")); // Assign test values
+    dis.setQuantity(new Scanner("7"));
+
+    double value = dis.calculate();
+
+    assertEquals("14.11", df.format(value), "Value should be 14.11");
+  }
+
+  @Test
+  void totalTest() {
+    dis.setDiscount(new Scanner("2"), dList);
+
+    dis.setUnitPrice(new Scanner("2.52")); // Assign test values
+    dis.setQuantity(new Scanner("7"));
+
+    dis.setDiscount(new Scanner("1"), dList);
+
+    dis.setUnitPrice(new Scanner("3")); // Assign test values
+    dis.setQuantity(new Scanner("2"));
+
+    double total = dis.calculate();
+    total += dis.calculate();
+
+    DecimalFormat df = new DecimalFormat("#.##");
+    assertEquals("19.51", df.format(total), "Value should be 19.51");
+  }
+
+  @Test
+  void wrongUserInputTest() {
+    dis.setUnitPrice(new Scanner("a"));
+    dis.setUnitPrice(new Scanner("b"));
+    dis.setUnitPrice(new Scanner("c")); // Incorrect inputs
+    dis.setUnitPrice(new Scanner("d"));
+
+    dis.setUnitPrice(new Scanner("2.52")); // Correct input
+
+    dis.setQuantity(new Scanner("a"));
+    dis.setQuantity(new Scanner("b")); // Incorrect input
+    dis.setQuantity(new Scanner("c"));
+    dis.setQuantity(new Scanner("d"));
+
+    dis.setQuantity(new Scanner("7")); // Correct input
+
+    dis.setDiscount(new Scanner("10000"), dList);
+    dis.setDiscount(new Scanner("-10000"), dList); // Incorrect inputs
+    dis.setDiscount(new Scanner("z"), dList);
+
+    dis.setDiscount(new Scanner("2"), dList); // Correct input
+
+    // Now lets check whether if the discount can still be calculated
+    double answer = dis.calculate();
+
+    DecimalFormat df = new DecimalFormat("#.##");
+    assertEquals("14.11", df.format(answer), "Value should be 14.11");
+  }
 }
