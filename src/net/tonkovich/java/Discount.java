a77bc23b1c6bf11406efb7871d0a3d96e56b20cb
Tonk <a_tonkovich@yahoo.com>
2018-02-04 01:03:14 -0600

    Cash register can now handle incorrect input

--- src/net/tonkovich/java/Discount.java
+++ src/net/tonkovich/java/Discount.java
@@ -1,18 +1,98 @@
 package net.tonkovich.java;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Discount {
-  
-  DiscountOptions d;
-  
-  public double calculate(double price, int quantity) {
-    return d.calculate(price, quantity);
+
+
+  private static Discount instance = new Discount();
+  private DiscountOptions d;
+  private double unitPrice = 0.0;
+  private int quantity = 0;
+
+  private Discount() {
+    // Change constructor to private
   }
-  
+
+
+  public static Discount getInstance() {
+    return instance;
+  }
+
+  /**
+   * After the behavior, unitPrice, and quantity have been assigned this method will use the
+   * assigned behaviors method to calculate the final price.
+   *
+   * @return Final price after discount
+   */
+  public double calculate() {
+    return d.calculate(unitPrice, quantity);
+  }
+
+  /**
+   * This method takes in user input, parses it, and assigns the behavior the user wants.
+   * If the user enters incorrect input the method will recurse and ask the user again
+   * indefinitely until correct input is entered.
+   *
+   * @param scan      Scanner object to get user input
+   * @param discounts List of discount options
+   */
+  public void setDiscount(Scanner scan, List<DiscountOptions> discounts) {
+    int selection;
+    try {
+      // Input comes from console expects a stream and waits for input if none or continue if is
+      if (scan.hasNext()) {
+        selection = Integer.parseInt(scan.next().trim());
+        if (selection <= discounts.size() && selection >= 1) {
+          d = discounts.get(selection - 1);
+        } else {
+          // Throw exception if selection outOfBounds
+          throw new Exception("Please choose an option presented.");
+        }
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      System.out.println("Incorrect selection: Try again");
+      setDiscount(scan, discounts); // Recursive call
+    }
+  }
+
+  /**
+   * This method takes in user input, parses it, and assigns the unit price for the item the
+   * user is trying to calculate. If the user inputs a price incorrectly the method will
+   * recurse indefinitely until correct input is entered.
+   *
+   * @param scan Scanner object to get user input
+   */
+  public void setUnitPrice(Scanner scan) {
+    try {
+      if (scan.hasNext()) // Used to avoid stack overflow, will still wait for user input
+        unitPrice = Double.parseDouble(scan.next().trim());
+    } catch (Exception e) {
+      System.out.println("Incorrect price format: Try again");
+      setUnitPrice(scan); // Recursive call
+    }
+  }
+
+  /**
+   * This method takes in user input, parses it, and assigns the quantity of the items the
+   * user is trying to calculate. If the user inputs a quantity incorrectly the method will
+   * recurse indefinitely until correct input is entered.
+   *
+   * @param scan Scanner object to get user input
+   */
+  public void setQuantity(Scanner scan) {
+    try {
+      if (scan.hasNext()) // Used to avoid stack overflow, will still wait for user input
+        quantity = Integer.parseInt(scan.next().trim());
+    } catch (Exception e) {
+      System.out.println("Incorrect quantity format: Try again");
+      setQuantity(scan);  // Recursive call
+    }
+  }
+
   public String toString() {
     return d.toString();
-  }
-  
-  public void setDiscount(DiscountOptions newD) {
-    d = newD;
   }
 }
--- src/net/tonkovich/java/Main.java
+++ src/net/tonkovich/java/Main.java
@@ -1,80 +1,60 @@
 package net.tonkovich.java;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+  private static Discount disc = Discount.getInstance();
+
+
+  /**
+   * Main class holds the procedure code for the cash register.
+   *
+   * @param args Command line arguments
+   */
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in); 
-    Discount disc = new Discount();
-    
-    List<DiscountOptions> discounts = new ArrayList<DiscountOptions>() {{
-      add(new Discount10());
-      add(new Discount20());
-    }};
-    
+    Scanner scan = new Scanner(System.in);
+
+    // Initialize discount options
+    List<DiscountOptions> discounts = new ArrayList<DiscountOptions>() {
+      {
+        add(new Discount10());
+        add(new Discount20());
+      }
+    };
+
     boolean cont = true;
     double total = 0.0;
-    
-    while(cont) {
+
+    while (cont) {
       System.out.println("Unit Price ($)");
-      double unitPrice = getUnitPrice(scan); // get UnitPrice
-      
-      System.out.println("Quantity");
-      int quantity = getQuantity(scan); // get Quantity of item
-      
+      disc.setUnitPrice(scan); // set Unit Price
+
+      System.out.println("Quantity: ");
+      disc.setQuantity(scan); // set Quantity of item
+
       System.out.println("Discounts: ");
-      
+
       int i = 1;
       for (DiscountOptions d : discounts) {
         System.out.println(i + ". " + d.toString()); // Print out discount options
         i++;
       }
-      int selection = scan.nextInt() -1 ;
-      
-      disc.setDiscount(discounts.get(selection)); // Assign behavior
-      
-      total += disc.calculate(unitPrice, quantity);
-      
+
+      disc.setDiscount(scan, discounts); // Assign behavior
+
+      total += disc.calculate(); // Accumulate total
+
       System.out.println("Total: ");
-      System.out.printf("%.2f \n", disc.calculate(unitPrice, quantity)); // Print out adjusted price
+      System.out.printf("%.2f \n", total); // Print out adjusted price
+
       System.out.println("Another item? (Y/N)");
-      
-      String answer = scan.next();
-      
-      if (!answer.equalsIgnoreCase("y"))
+      if (!scan.next().equalsIgnoreCase("y")) {
         cont = false;
+      }
     }
-    scan.close();
+    scan.close(); // Close scanner
   }
-  
-  public static double getUnitPrice(Scanner scan) {
-  	double result = 0.0; // Default
-  	try {
-  		result = scan.nextDouble();
-  	} 
-  	catch (InputMismatchException e) {
-  		System.out.println("Incorrect number format: Try again");
-  		String result2 = scan.nextLine();
-  		getUnitPrice(new Scanner(result2));
-  	}
-  	return result;
-  }
-  
-  public static int getQuantity(Scanner scan) {
-  	int result = 0;
-  	try {
-  		result = scan.nextInt();
-  	} 
-  	catch (InputMismatchException e) {
-  		System.out.println("Incorrect number format: Try again");
-  		getQuantity(new Scanner(scan.next()));
-  	}
-  	return result;
-  }
-  
 }
